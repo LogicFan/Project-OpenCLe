e9b6bfc2026129dafda4f9871b2d70cb45090838
LogicFan <38597904+LogicFan@users.noreply.github.com>
2019-01-21 00:55:34 -0500

    add vendor def

---
+++ src/coredef.hpp
@@ -0,0 +1,13 @@
+#ifndef COREDEF_HPP
+#define COREDEF_HPP
+
+namespace opencle
+{
+namespace Vendor
+{
+char const Nvidia[] = "NVIDIA Corporation";
+char const AMD[] = "Advanced Micro Devices, Inc.";
+}
+} // namespace oepncle
+
+#endif
