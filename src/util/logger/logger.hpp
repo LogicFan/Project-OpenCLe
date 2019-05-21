e16338bac22adf09d89f7673b1ef9a1f1efac8cf
LogicFan <fanyongda2012@gmail.com>
2019-05-20 21:45:05 -0500

    Format

--- src/util/logger/logger.hpp
+++ src/util/logger/logger.hpp
@@ -1,3 +1,5 @@
-#include <iostream>
 #include <iomanip>
-#define logger std::cout << "[" << std::left << std::setw(20) << __FILE__ << ", " << std::setw(4) << __LINE__ << "] " << std::right
+#include <iostream>
+#define logger                                                                 \
+    std::cout << "[" << std::left << std::setw(20) << __FILE__ << ", "         \
+              << std::setw(4) << __LINE__ << "] " << std::right
