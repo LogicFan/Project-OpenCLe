9fe2ee6e67321730541e00cbc2cb0130e36e0e14
LogicFan <fanyongda2012@gmail.com>
2019-05-21 13:38:44 -0500

    add clone method

--- src/memory/global_ptr_impl.hpp
+++ src/memory/global_ptr_impl.hpp
@@ -7,62 +7,61 @@ namespace opencle {
 class device;
 
 class global_ptr_impl final {
-    private:
-    /** Host-side memory management **/
-    void *host_ptr_;
+  private:
     size_t size_;
+
+    /** host-side memory */
+    void *host_ptr_;
     std::function<void(void *)> deleter_;
 
-    /** Device-side memory management **/
+    /** device-side memory */
     mutable cl_mem device_ptr_;
     mutable device const *on_device_;
 
-    /** Indicate if this is a write-only memory.
-     * This is true if object is initialized by 
-     * global_ptr_impl(size_t) and never called 
-     * get() method **/
+    /** a memory is write only if write_only_ is true; a memory is read only if
+     * it is const */
     bool write_only_;
 
-    /** If the object is const object, then it 
-     * is a read_only memory **/
+  public:
 
-    public:
-    /** For global_ptr **/
-
-    /** Create a write only memory **/
+    /** write only memory constructor */
     global_ptr_impl(size_t size);
 
-    /** Create a general memory **/
-    global_ptr_impl(void *ptr, size_t size, std::function<void(void *)> deleter);
-    
-    /** Can not be copied **/
+    /** general memory constructor */
+    global_ptr_impl(void *ptr, size_t size,
+                    std::function<void(void *)> deleter);
+
     global_ptr_impl(global_ptr_impl const &rhs) = delete;
-    
-    /** Move constructor **/
+
+    /** move constructor */
     global_ptr_impl(global_ptr_impl &&rhs);
-    
-    /** Destructor **/
+
+    /** destructor */
     ~global_ptr_impl();
 
-    /** Can not be copied **/
     global_ptr_impl &operator=(global_ptr_impl const &rhs) = delete;
-    
-    /** Move assignment operator **/
+
+    /** move assignment operator */
     global_ptr_impl &operator=(global_ptr_impl &&rhs);
-    
-    /** Enqueue memory to host-side and then return 
-     * the address. This will cause write_only_ to 
-     * be false. **/
+
+
+    /** enqueue memory to host-side and return
+     * the address. This will cause write_only_ to
+     * be false. */
     void *get();
+    void const *get() const;
 
-    /** Return the host-side memory address **/
-    void *get() const;
-
-    /** Similar to get, but also move ownership **/
+    /** similar to get, but also move ownership */
     void *release();
 
-    /** Return true if memory is not empty. **/
+    /** return a deep copy */
+    global_ptr_impl clone() const;
+
+    /** return true if memory is not empty. */
     operator bool() const;
+
+    /** return the size */
+    size_t size();
 
     /** For device **/
 
@@ -70,4 +69,4 @@ class global_ptr_impl final {
     cl_mem to_device(device const &dev);
     cl_mem to_device(device const &dev) const;
 };
-}
+} // namespace opencle
