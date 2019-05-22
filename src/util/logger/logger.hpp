010d29eb04ca2f727c7c5e39518c774c80be9663
LogicFan <fanyongda2012@gmail.com>
2019-05-21 22:08:31 -0500

    format

--- src/util/logger/logger.hpp
+++ src/util/logger/logger.hpp
@@ -1,15 +1,17 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+
 #include "../core_def.hpp"
 
 #ifndef NDEBUG
 #define logger                                                                 \
     std::cout << "[" << std::left << std::setw(20) << __FILE__ << ", "         \
               << std::setw(4) << __LINE__ << "] " << std::right
-#else 
+#else
 static std::stringstream _logger_ndebug_stringstream_;
-inline std::stringstream &_func_logger_ndebug_stringstream_(std::stringstream &ss) {
+inline std::stringstream &
+_func_logger_ndebug_stringstream_(std::stringstream &ss) {
     ss.clear();
     return ss;
 }
