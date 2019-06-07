0dfb719bbaad8739ac72f38f3667415401d75549
LogicFan <fanyongda2012@gmail.com>
2019-06-06 21:33:31 -0400

    Update global_ptr_impl.hpp

--- src/memory/global_ptr_impl.hpp
+++ src/memory/global_ptr_impl.hpp
@@ -6,6 +6,8 @@
 namespace opencle {
 class device;
 
+/** const indicate read_only, otherwise is read_write **/
+
 class global_ptr_impl final {
   private:
     size_t size_;
@@ -18,54 +20,33 @@ class global_ptr_impl final {
     mutable cl_mem device_ptr_;
     mutable device const *on_device_;
 
-    /** a memory is write only if write_only_ is true; a memory is read only if
-     * it is const */
-    bool write_only_;
-
   public:
-
-    /** write only memory constructor */
     global_ptr_impl(size_t size);
 
-    /** general memory constructor */
+    /* do not copy the data */
     global_ptr_impl(void *ptr, size_t size,
                     std::function<void(void *)> deleter);
-
     global_ptr_impl(global_ptr_impl const &rhs) = delete;
-
-    /** move constructor */
     global_ptr_impl(global_ptr_impl &&rhs);
-
-    /** destructor */
     ~global_ptr_impl();
 
     global_ptr_impl &operator=(global_ptr_impl const &rhs) = delete;
 
-    /** move assignment operator */
     global_ptr_impl &operator=(global_ptr_impl &&rhs);
 
-
-    /** enqueue memory to host-side and return
-     * the address. This will cause write_only_ to
-     * be false. */
     void *get();
     void const *get() const;
 
-    /** similar to get, but also move ownership */
     void *release();
+    void const *release() const;
 
-    /** return a deep copy */
     global_ptr_impl clone() const;
 
-    /** return true if memory is not empty. */
     operator bool() const;
-
-    /** return the size */
     size_t size();
 
     /** For device **/
 
-    /** Enqueue memory to device-side dev **/
     cl_mem to_device(device const &dev);
     cl_mem to_device(device const &dev) const;
 };
