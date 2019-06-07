f62454ce5e13b572341a5e1ecb5f2852001e82fd
LogicFan <fanyongda2012@gmail.com>
2019-06-06 23:16:50 -0400

    Implement constructor and desturctor

--- src/memory/global_ptr_impl.cpp
+++ src/memory/global_ptr_impl.cpp
@@ -1,81 +1,57 @@
 #include "global_ptr_impl.hpp"
-
-#define GLOBAL_PTR_IMPL_TEST
-
-#ifndef GLOBAL_PTR_IMPL_TEST
-#include "../device/device.hpp"
-
-#else
-#include <atomic>
-#include <iostream>
-namespace opencle {
-struct device final {
-    mutable std::atomic<bool> valid_;
-
-    cl_device_id device_;
-    cl_context context_;
-    cl_command_queue c_queue_;
-
-    const size_t cu_total_;
-    mutable std::atomic<size_t> cu_used_;
-
-    device()
-        : valid_{true}, device_{nullptr}, context_{nullptr}, c_queue_{nullptr},
-          cu_total_{0}, cu_used_{0} {}
-};
-} // namespace opencle
-#endif
-
+#include "../device/device_impl.hpp"
 #include <assert.h>
 #include <stdexcept>
 #include <stdlib.h>
 
 namespace opencle {
+
 global_ptr_impl::global_ptr_impl(size_t size)
-    : host_ptr_{nullptr}, size_{size}, deleter_{}, device_ptr_{nullptr},
-      on_device_{nullptr}, write_only_{true} {
+    : global_ptr_impl{new char[size], size,
+                      [](void const *ptr) { delete[] ptr; }} {
     return;
 }
 
 global_ptr_impl::global_ptr_impl(void *ptr, size_t size,
-                                 std::function<void(void *)> deleter)
-    : host_ptr_{ptr}, size_{size}, deleter_{deleter}, device_ptr_{nullptr},
-      on_device_{nullptr}, write_only_{false} {
+                                 std::function<void(void const *)> deleter)
+    : size_{size}, host_ptr_{ptr}, deleter_{deleter}, device_ptr_{nullptr},
+      on_device_{nullptr} {
     return;
 }
 
 global_ptr_impl::global_ptr_impl(global_ptr_impl &&rhs)
-    : host_ptr_{rhs.host_ptr_}, size_{rhs.size_}, deleter_{rhs.deleter_},
-      device_ptr_{rhs.device_ptr_}, on_device_{rhs.on_device_},
-      write_only_{rhs.write_only_} {
+    : size_{rhs.size_}, host_ptr_{rhs.host_ptr_}, deleter_{rhs.deleter_},
+      device_ptr_{rhs.device_ptr_}, on_device_{rhs.on_device_} {
     rhs.host_ptr_ = nullptr;
     rhs.device_ptr_ = nullptr;
-    rhs.on_device_ = nullptr;
+    return;
 }
 
 global_ptr_impl::~global_ptr_impl() {
-    if (this->host_ptr_) {
-        this->deleter_(this->host_ptr_);
-        this->host_ptr_ = nullptr;
+    if (host_ptr_) {
+        deleter_(this->host_ptr_);
     }
 
-    if (this->device_ptr_) {
+    if (device_ptr_) {
         clReleaseMemObject(this->device_ptr_);
-        this->device_ptr_ = nullptr;
-        this->on_device_ = nullptr;
     }
 }
 
 global_ptr_impl &global_ptr_impl::operator=(global_ptr_impl &&rhs) {
     this->~global_ptr_impl();
 
-    assert(!this->device_ptr_);
-    assert(!this->host_ptr_);
-    assert(!this->on_device_);
-
     new (this) global_ptr_impl(std::move(rhs));
     return *this;
 }
+
+// cl_mem global_ptr_impl::to_device(device const &dev) {
+//     if (on_device_ == &dev) {
+//         return device_ptr_;
+//     } else if {
+//     }
+// }
+
+#if 0
 
 void *global_ptr_impl::get() {
     this->write_only_ = false;
@@ -232,148 +208,6 @@ cl_mem global_ptr_impl::to_device(device const &dev) const {
     return nullptr;
 }
 
-} // namespace opencle
-
-#ifdef GLOBAL_PTR_IMPL_TEST
-
-#include <CL/cl.h>
-#include <stdlib.h>
-
-const char *programSource =
-    "__kernel \n"
-    "void vecadd(__global int *A, __global int *B, __global int *C) \n"
-    "{ \n"
-    " int idx = get_global_id(0); \n"
-    " C[idx] = A[idx] + B[idx]; \n"
-    "} \n";
-int main() {
-    constexpr int elements = 16;
-    size_t datasize = sizeof(int) * elements;
-
-    // Allocate space for input/output host data
-    int *A = (int *)malloc(datasize); // Input array
-    int *B = (int *)malloc(datasize); // Input array
-    int *C = (int *)malloc(datasize); // Output array
-
-    // Initialize the input data
-    for (int i = 0; i < elements; i++) {
-        A[i] = i;
-        B[i] = i;
-    }
-
-    cl_int status;
-
-    cl_platform_id platform;
-    cl_uint num;
-    status = clGetPlatformIDs(0, NULL, &num);
-    if (status == CL_SUCCESS && num > 0) {
-        status = clGetPlatformIDs(1, &platform, NULL);
-    } else {
-        throw std::runtime_error{""};
-    }
-
-    cl_device_id device;
-    status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, NULL, &num);
-    if (status == CL_SUCCESS && num > 0) {
-        status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 1, &device, NULL);
-    } else {
-        throw std::runtime_error{""};
-    }
-
-    cl_context context = clCreateContext(NULL, 1, &device, NULL, NULL, &status);
-
-    cl_command_queue cmdQueue =
-        clCreateCommandQueue(context, device, NULL, &status);
-
-    // opencle::device dev;
-    // dev.c_queue_ = cmdQueue;
-    // dev.context_ = context;
-    // dev.device_ = device;
-    // dev.valid_ = true;
-
-    // cl_mem bufA =
-    //     clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
-    // cl_mem bufB =
-    //     clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
-    // cl_mem bufC =
-    //     clCreateBuffer(context, CL_MEM_WRITE_ONLY, datasize, NULL, &status);
-    //
-    // status = clEnqueueWriteBuffer(cmdQueue, bufA, CL_FALSE, 0, datasize, A,
-    // 0,
-    //                               NULL, NULL);
-    // status = clEnqueueWriteBuffer(cmdQueue, bufB, CL_FALSE, 0, datasize, B,
-    // 0,
-    //                               NULL, NULL);
-
-    // opencle::global_ptr_impl ptrA{A, datasize, free};
-    // opencle::global_ptr_impl ptrB{B, datasize, free};
-    // opencle::global_ptr_impl ptrC{datasize};
-
-    cl_mem bufA =
-        clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
-    cl_mem bufB =
-        clCreateBuffer(context, CL_MEM_READ_ONLY, datasize, NULL, &status);
-    cl_mem bufC =
-        clCreateBuffer(context, CL_MEM_WRITE_ONLY, datasize, NULL, &status);
-
-    status = clEnqueueWriteBuffer(cmdQueue, bufA, CL_TRUE, 0, datasize, A, 0,
-                                  NULL, NULL);
-    status = clEnqueueWriteBuffer(cmdQueue, bufB, CL_TRUE, 0, datasize, B, 0,
-                                  NULL, NULL);
-
-    // Create a program with source code
-    cl_program program = clCreateProgramWithSource(
-        context, 1, (const char **)&programSource, NULL, &status);
-    // Build(compile) the program for the device
-    status = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
-    // Create the vector addition kernel
-    cl_kernel kernel = clCreateKernel(program, "vecadd", &status);
-    // Set the kernel arguments
-
-    // std::cout << "A: ";
-    // cl_mem bufA = ptrA.to_device(dev);
-    // std::cout << "B: ";
-    // cl_mem bufB = ptrB.to_device(dev);
-    // std::cout << "C: ";
-    // cl_mem bufC = ptrC.to_device(dev);
-
-    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufA);
-    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufB);
-    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufC);
-
-    // Define an incde space of work-items for execution
-    // A work-group size is not required, but can be used.
-    size_t indexSpaceSize[1], workGroupSize[1];
-    // There are 'elements' work-items
-    indexSpaceSize[0] = elements;
-    workGroupSize[0] = 4;
-    // Execute the kernel
-    status = clEnqueueNDRangeKernel(cmdQueue, kernel, 1, NULL, indexSpaceSize,
-                                    workGroupSize, 0, NULL, NULL);
-    // Read the device output buffer to the host output array
-    // std::cout << "C: ";
-    // int *C = static_cast<int *>(ptrC.get());
-
-    status = clEnqueueReadBuffer(cmdQueue, bufC, CL_TRUE, 0, datasize, C, 0,
-                                 NULL, NULL);
-
-    for (int i = 0; i < elements; ++i) {
-        std::cout << *(C + i) << std::endl;
-    }
-
-    // Free OpenCL resouces
-    clReleaseKernel(kernel);
-    clReleaseProgram(program);
-    clReleaseCommandQueue(cmdQueue);
-    clReleaseMemObject(bufA);
-    clReleaseMemObject(bufB);
-    clReleaseMemObject(bufC);
-    clReleaseContext(context);
-    // free host resouces
-    free(A);
-    free(B);
-    free(C);
-
-    return 0;
-}
 #endif
+
+} // namespace opencle
