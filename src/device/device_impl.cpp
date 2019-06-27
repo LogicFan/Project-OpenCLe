82f180f68c0ae4497eebc531870a260ac5ed8d88
LogicFan <fanyongda2012@gmail.com>
2019-06-26 21:28:52 -0400

    Refactor device_impl (test is not update yet)

--- src/device/device_impl.cpp
+++ src/device/device_impl.cpp
@@ -1,172 +1,147 @@
 #include <iostream>
 #include <stdexcept>
-#include <utility>
 #include <type_traits>
+#include <utility>
 
 #define NDEBUG
 
-#include "device_impl.hpp"
 #include "../memory/global_ptr.hpp"
+#include "device_impl.hpp"
 
-namespace {
-void pfn_notify(const char *errinfo, const void *private_info, size_t cb, void *user_data) {
-    logger("pfn_notify");
+namespace
+{
+void __pfn_notify(const char *errinfo, const void *private_info, size_t cb, void *user_data)
+{
+    logger("__pfn_notify(const char *, const void *, size_t, void *)");
 
     throw std::runtime_error{"OpenCL runtime error: Context failure. " + std::string{errinfo}};
 }
 
-cl_context get_context(cl_device_id const &dev) {
-    logger("get_context");
+cl_context __get_context(cl_device_id const &dev_id)
+{
+    logger("__get_context(cl_device_id const &)");
     cl_int status;
-    cl_context temp = clCreateContext(NULL, 1, &dev, pfn_notify, NULL, &status);
-    if (status != CL_SUCCESS) {
+    cl_context temp = clCreateContext(NULL, 1, &dev_id, __pfn_notify, NULL, &status);
+    if (status != CL_SUCCESS)
+    {
         throw std::runtime_error{"OpenCL runtime error: Cannot initialize context!"};
     }
-    logger("Create context " << temp << " based on device_id " << dev << "!");
+    logger("Create context " << temp << " based on device_id " << dev_id);
 
     return temp;
 }
 
-cl_command_queue get_command_queue(cl_device_id const &dev, cl_context const &con) {
-    logger("get_command_queue");
+cl_command_queue __get_command_queue(cl_device_id const &dev_id, cl_context const &context)
+{
+    logger("__get_command_queue(cl_device_id const &, cl_context const &)");
     cl_int status;
-    cl_command_queue temp = clCreateCommandQueueWithProperties(con, dev, NULL, &status);
-    if (status != CL_SUCCESS) {
+    cl_command_queue temp = clCreateCommandQueueWithProperties(context, dev_id, NULL, &status);
+    if (status != CL_SUCCESS)
+    {
         throw std::runtime_error{"OpenCL runtime error: Cannot initialize command queue!"};
     }
-    logger("Create command queue " << temp << " based on context " << con << " and device_id " << dev << "!");
+    logger("Create command queue " << temp << " based on context " << context << " and device_id " << dev_id);
 
     return temp;
 }
 
-size_t get_computate_unit(cl_device_id const &dev) {
-    logger("get_computate_unit");
+size_t __get_computate_unit(cl_device_id const &dev_id)
+{
+    logger("__get_computate_unit(cl_device_id const &)");
     cl_int status;
     cl_uint cu_num;
-    status = clGetDeviceInfo(dev, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_uint), &cu_num, NULL);
-    if (status != CL_SUCCESS) {
+    status = clGetDeviceInfo(dev_id, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_uint), &cu_num, NULL);
+    if (status != CL_SUCCESS)
+    {
         throw std::runtime_error{"OpenCL runtime error: Cannot get device info!"};
     }
-    logger("There are " << cu_num << " number of compute unit on device where device_id is " << dev << "!");
+    logger("There are " << cu_num << " number of compute unit on device where device_id is " << dev_id);
 
     return static_cast<size_t>(cu_num);
 }
 } // namespace
 
-namespace opencle {
-device_impl::device_impl(cl_device_id const &dev, cl_context const &con, cl_command_queue const &cmd)
-    : device_{dev}, context_{con}, cmd_queue_{cmd}, cu_total{get_computate_unit(dev)}, valid_{true}, cu_used{0} {
-    logger("device_impl(device_id, context, command_queue), create " << this);
+namespace opencle
+{
+device_impl::device_impl(cl_device_id const &dev_id)
+    : device_{dev_id}, context_{__get_context(device_)},
+      cmd_queue_{__get_command_queue(device_, context_)}, cu_total{__get_computate_unit(device_)}, valid_{true}, cu_used{0}
+{
+    logger("device_impl(device_id const &), create " << this);
     return;
 }
 
-device_impl::device_impl(cl_device_id const &dev)
-    : device_{dev}, context_{get_context(device_)},
-      cmd_queue_{get_command_queue(device_, context_)}, cu_total{get_computate_unit(dev)}, valid_{true}, cu_used{0} {
-    logger("device_impl(device_id), create " << this);
+device_impl::device_impl(cl_device_id const &dev_id, cl_context const &context, cl_command_queue const &cmd_q)
+    : device_{dev_id}, context_{context}, cmd_queue_{cmd_q}, cu_total{__get_computate_unit(dev_id)}, valid_{true}, cu_used{0}
+{
+    logger("device_impl(device_id const &, context const &, command_queue const &), create " << this);
     return;
 }
 
-// device_impl::device_impl(device_impl &&rhs)
-//     : device_{rhs.device_}, context_{rhs.context_},
-//       cmd_queue_{rhs.cmd_queue_}, cu_total{rhs.cu_total},
-//       valid_{rhs.valid_.load()}, cu_used{rhs.cu_used.load()} {
-//     rhs.device_ = nullptr;
-//     rhs.context_ = nullptr;
-//     rhs.cmd_queue_ = nullptr;
-//     rhs.valid_ = false;
-// }
-
-device_impl::~device_impl() {
-    logger("~device_impl, destory " << this);
+device_impl::~device_impl()
+{
+    logger("~device_impl(), destory " << this);
     clReleaseCommandQueue(cmd_queue_);
     logger("Release command queue " << cmd_queue_);
     clReleaseContext(context_);
     logger("Release context " << context_);
 }
 
-// device_impl &device_impl::operator=(device_impl &&rhs) {
-//     this->~device_impl();
-//     device_impl(std::move(rhs));
-//     return *this;
-// }
-
-bool device_impl::operator<(device_impl const &rhs) const {
-    logger("operator<, " << this << " and " << &rhs);
-    return !valid_ && cu_total - cu_used < rhs.cu_total - rhs.cu_used;
+bool device_impl::operator<(device_impl const &rhs) const
+{
+    logger("operator<(device_impl const &) const, compare " << this << " and " << &rhs);
+    return valid_ * (cu_total - cu_used) < rhs.valid_ * (rhs.cu_total - rhs.cu_used);
 }
 
-device_impl::operator bool() { 
-    logger("operator bool")
-    return valid_; 
+device_impl::operator bool() const
+{
+    logger("operator bool() const");
+    return valid_;
 }
 
-std::vector<device> device_impl::get_device_list() {
-    logger("get_device_list");
-    std::vector<device> device_list;
-
-    cl_int status;
-
-    cl_uint platform_num;
-    status = clGetPlatformIDs(0, NULL, &platform_num);
-    if (status != CL_SUCCESS) {
-        throw std::runtime_error{"OpenCL runtime error: Cannot initialize platform!"};
-    } else if (platform_num == 0) {
-        throw std::out_of_range{"No platforms detected!"};
-    }
-
-    logger("Detect " << platform_num << " number of platforms!");
-
-    std::unique_ptr<cl_platform_id[]> platforms{new cl_platform_id[platform_num]};
-    status = clGetPlatformIDs(platform_num, platforms.get(), NULL);
-    if (status != CL_SUCCESS) {
-        throw std::runtime_error{"OpenCL runtime error: Cannot initialize platform!"};
-    }
-
-    for (int i = 0; i < platform_num; ++i) {
-        logger("In platform " << platforms[i] << "!");
-
-        cl_uint device_num;
-        status = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &device_num);
-        if (status != CL_SUCCESS) {
-            throw std::runtime_error{"OpenCL runtime error: Cannot initialize device"};
-        } else if (device_num == 0) {
-            throw std::out_of_range{"Cannot detect devices"};
-        }
-
-        logger("Detect " << device_num << " number of devices!");
-
-        std::unique_ptr<cl_device_id[]> devices{new cl_device_id[device_num]};
-
-        status = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, device_num, devices.get(), NULL);
-
-        if (status != CL_SUCCESS) {
-            throw std::runtime_error{"OpenCL runtime error: Cannot initialize device"};
-        }
-
-        for (int j = 0; j < device_num; ++j) {
-            device_list.emplace_back(new device_impl{devices[j]});
-            logger("Initialize device with device_id " << devices[j] << "!");
-        }
-    }
-
-    return std::move(device_list);
+cl_device_id device_impl::get_device_id() const
+{
+    logger("get_device_id() const");
+    return device_;
 }
 
-std::ostream &operator<<(std::ostream &out, device_impl const &dev) {
+cl_context device_impl::get_context() const
+{
+    logger("get_context() const");
+    return context_;
+}
+
+cl_command_queue device_impl::get_command_queue() const
+{
+    logger("get_command_queue() const");
+    return cmd_queue_;
+}
+
+size_t device_impl::get_computate_unit_available() const
+{
+    logger("get_computate_unit_available() const");
+    return cu_total - cu_used;
+}
+
+void device_impl::computate_unit_usage_increment(int offset) { 
+    logger("computate_unit_usage_increment(int)"); 
+    cu_used = cu_used + offset;
+}
+
+std::ostream &operator<<(std::ostream &out, device_impl const &dev_impl)
+{
     cl_int status;
 
     char device_name[100];
-    status = clGetDeviceInfo(dev.device_, CL_DEVICE_NAME, 100, device_name, NULL);
+    status = clGetDeviceInfo(dev_impl.device_, CL_DEVICE_NAME, 100, device_name, NULL);
 
-    if (status != CL_SUCCESS) {
+    if (status != CL_SUCCESS)
+    {
         throw std::runtime_error{"OpenCL runtime error: Cannot get device info"};
     }
 
-    out << "Device: " << device_name << ", " << &dev;
+    out << "Device: " << device_name << ", " << &dev_impl;
     return out;
 }
-
-std::ostream &operator<<(std::ostream &out, device const &dev) { return operator<<(out, *dev); }
 
 } // namespace opencle
--- src/device/device_impl.hpp
+++ src/device/device_impl.hpp
@@ -5,20 +5,17 @@
 #include <memory>
 #include <vector>
 
+#include "../memory/global_ptr.hpp"
 #include "../util/core_def.hpp"
 #include "../util/logger/logger.hpp"
-#include "../memory/global_ptr.hpp"
 
-namespace opencle_test {
-void test();
-};
-
-namespace opencle {
+namespace opencle
+{
 class device_impl;
-using device = std::unique_ptr<device_impl>;
 
-class device_impl final {
-    // private:
+class device_impl final
+{
+private:
     cl_device_id device_;
     cl_context context_;
     cl_command_queue cmd_queue_;
@@ -28,37 +25,25 @@ class device_impl final {
     mutable std::atomic<bool> valid_;
     mutable std::atomic<size_t> cu_used;
 
-    device_impl(cl_device_id const &dev, cl_context const &con, cl_command_queue const &cmd);
-    device_impl(cl_device_id const &dev);
-
 public:
+    device_impl(cl_device_id const &dev_id);
+    device_impl(cl_device_id const &dev_id, cl_context const &context, cl_command_queue const &cmd_q);
     device_impl(device_impl const &rhs) = delete;
     device_impl(device_impl &&rhs) = delete;
-
     ~device_impl();
 
     device_impl &operator=(device_impl const &rhs) = delete;
     device_impl &operator=(device_impl &&rhs) = delete;
     bool operator<(device_impl const &rhs) const;
-    operator bool();
+    operator bool() const;
 
-    static std::vector<device> get_device_list();
+    cl_device_id get_device_id() const;
+    cl_context get_context() const;
+    cl_command_queue get_command_queue() const;
+    size_t get_computate_unit_available() const;
 
-    template <typename T>
-    cl_mem synchronize(global_ptr<T[], void> const &memory) {
-        logger("synchronize");
-        return memory.impl_->to_device(this);
-    }
+    void computate_unit_usage_increment(int offset);
 
-    // void exec(task &&task);
-
-    friend std::ostream &operator<<(std::ostream &out, device_impl const &dev);
-    friend class global_ptr_impl;
-
-    friend void ::opencle_test::test();
+    friend std::ostream &operator<<(std::ostream &out, device_impl const &dev_impl);
 };
-
-std::ostream &operator<<(std::ostream &out, device_impl const &dev);
-
-std::ostream &operator<<(std::ostream &out, device const &dev);
 } // namespace opencle
